885b1e5364e2ae24ac9029f78c1bcf48cb5ccf61
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-08 12:05:50 +0330

    spread in array literals

--- jsFeatures.js
+++ jsFeatures.js
@@ -27,3 +27,20 @@ giveMeFour(...colors);
 
 const str = "GOAT";
 giveMeFour(...str);
+
+// spread in array literals
+const cephalopods = [
+  "dumbo octopus",
+  "humboldt squid",
+  "flamboyant cuttlefish",
+];
+
+const gastropods = ["giant african snail", "banana slug", "variable neon slug"];
+
+const cnidaria = ["fire coral", "moon jelly"];
+
+const mollusca = ["garden slug", ...cephalopods, ...gastropods];
+
+const inverts = [...cnidaria, ...gastropods, ...cephalopods];
+
+const cephCopy = [...cephalopods];
