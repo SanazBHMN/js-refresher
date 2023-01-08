b90c51f1adfafe38267d396c918cc1cf93808157
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-08 11:52:25 +0330

    spread for function calls

--- jsFeatures.js
+++ jsFeatures.js
@@ -12,3 +12,18 @@ function multiply(a, b = 1) {
 const greet = (person, greeting = "Hi") => {
   console.log(`${greeting}, ${person}!`);
 };
+
+// spread for function calls
+function giveMeFour(a, b, c, d) {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+  console.log("d", d);
+}
+
+const colors = ["red", "orange", "yellow", "green"];
+// giveMeFour(colors);
+giveMeFour(...colors);
+
+const str = "GOAT";
+giveMeFour(...str);
