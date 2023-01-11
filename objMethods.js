34fd93bfe961b48142d9246c47d34fa310c6c34d
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-11 15:37:46 +0330

    Shorthand object properties

---
+++ objMethods.js
@@ -0,0 +1,18 @@
+// shorthand object properies
+const getStats = (arr) => {
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  const sum = arr.reduce((sum, r) => sum + r);
+  const avg = sum / arr.length;
+
+  return {
+    max,
+    min,
+    sum,
+    avg,
+  };
+};
+
+const reviews = [4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5];
+
+const stats = getStats(reviews);
