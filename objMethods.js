195ff87c6d7de36f21967a82d0dc37f412f4033c
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-11 16:07:33 +0330

    method shorthand syntax

--- objMethods.js
+++ objMethods.js
@@ -60,3 +60,14 @@ const math = {
     return x * y;
   },
 };
+
+// method shorthand syntax
+const auth = {
+  username: "TommyBot",
+  login() {
+    console.log("LOGGED YOU IN!");
+  },
+  logout() {
+    console.log("GOODBYE!");
+  },
+};
