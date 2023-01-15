c17663fd26ec5b38f29aed72e7c6b26144b714d3
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-15 15:34:54 +0330

    annoyomatic demo

--- objMethods.js
+++ objMethods.js
@@ -105,3 +105,29 @@ const person = {
 
 // THIS: invocation context
 const printBio = person.printBio;
+
+// annoyomatic demo
+const annoyer = {
+  phrases: [
+    "literally",
+    "cray cray",
+    "I can't even",
+    "Totes!",
+    "YOLO",
+    "Can't Stop, Won't Stop",
+  ],
+  pickPhrase() {
+    const { phrases } = this;
+    const idx = Math.floor(Math.random() * phrases.length);
+    return phrases[idx];
+  },
+  start() {
+    this.timerId = setInterval(() => {
+      console.log(this.pickPhrase());
+    }, 3000);
+  },
+  stop() {
+    clearInterval(this.timerId);
+    console.log("PHEW THANK HEAVENS THAT IS OVER!");
+  },
+};
