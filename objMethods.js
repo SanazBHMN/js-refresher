2a910206fd5701f032d1b9b8cc1cb5c0feab5c60
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-15 16:24:59 +0330

    creating a deck factory

--- objMethods.js
+++ objMethods.js
@@ -135,40 +135,54 @@ const annoyer = {
 // putting it all together:
 // Deck of Cards
 
-const myDeck = {
-  deck: [],
-  drawnCards: [],
-  suits: ["hearts", "diamonds", "spades", "clubs"],
-  values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
-  initializeDeck() {
-    const { suits, values, deck } = this;
-    for (let value of values.split(",")) {
-      for (let suit of suits) {
-        deck.push({ value, suit });
+// creating a deck factory
+const makeDeck = () => {
+  return {
+    deck: [],
+    drawnCards: [],
+    suits: ["hearts", "diamonds", "spades", "clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    initializeDeck() {
+      const { suits, values, deck } = this;
+      for (let value of values.split(",")) {
+        for (let suit of suits) {
+          deck.push({ value, suit });
+        }
       }
-    }
-    // return deck;
-  },
-  drawCard() {
-    const card = this.deck.pop();
-    this.drawnCards.push(card);
-    return card;
-  },
-  drawMultiple(numCards) {
-    const cards = [];
-    for (let i = 0; i < numCards; i++) {
-      cards.push(this.drawCard());
-    }
-    return cards;
-  },
-  shuffle() {
-    const { deck } = this;
-    // loop over array backwards
-    for (let i = deck.length - 1; i > 0; i--) {
-      // pick random index before current element
-      let j = Math.floor(Math.random() * (i + 1));
-      // swap
-      [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-  },
+      // return deck;
+    },
+    drawCard() {
+      const card = this.deck.pop();
+      this.drawnCards.push(card);
+      return card;
+    },
+    drawMultiple(numCards) {
+      const cards = [];
+      for (let i = 0; i < numCards; i++) {
+        cards.push(this.drawCard());
+      }
+      return cards;
+    },
+    shuffle() {
+      const { deck } = this;
+      // loop over array backwards
+      for (let i = deck.length - 1; i > 0; i--) {
+        // pick random index before current element
+        let j = Math.floor(Math.random() * (i + 1));
+        // swap
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+      }
+    },
+  };
 };
+
+// much cleaner
+const myDeck = makeDeck();
+myDeck.initializeDeck();
+myDeck.shuffle();
+const h1 = myDeck.drawMultiple(2);
+const h2 = myDeck.drawMultiple(2);
+const h3 = myDeck.drawMultiple(5);
+
+const deck2 = makeDeck();
+deck2.initializeDeck();
