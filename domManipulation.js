767c6520cc62f258aee66759d90a5a8a22d6ecbf
SanazBHMN <sanazbahmani35@gmail.com>
2023-01-22 10:55:08 +0330

    querySelector & querySelectorAll

--- domManipulation.js
+++ domManipulation.js
@@ -1,5 +1,5 @@
 // getElementsByTagName
-let inputs = document.getElementsByTagName("input");
+let inputs = document.getElementsByTagName("input"); // HTMLCollection
 
 for (let input of inputs) {
   // returns an array-like object
@@ -14,3 +14,13 @@ const special = document.getElementsByClassName("special");
 
 const ul = document.getElementsByTagName("ul")[0];
 console.log(ul.getElementsByClassName("special"));
+
+// querySelector & querySelectorAll
+// main1 and main2 are both HTMLParagraphElement
+const main1 = document.querySelector("#main");
+const main2 = document.getElementsByTagName("main");
+
+console.dir(document.querySelector("section ul li.special"));
+console.dir(document.querySelector("input[type='password']"));
+
+const allInputs = document.querySelectorAll("input"); // NodeList
