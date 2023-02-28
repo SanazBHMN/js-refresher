60fca126dbd085d871969e6911bb16b45c92810c
SanazBHMN <sanazbahmani35@gmail.com>
2023-02-28 13:15:52 +0330

    Key events: keypress,keyup, & keydown

--- app.js
+++ app.js
@@ -14,45 +14,75 @@
 //   document.body.style.backgroundColor = "green";
 // });
 
-const colors = [
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "purple",
-  "indigo",
-  "violet",
-];
+// const colors = [
+//   "red",
+//   "orange",
+//   "yellow",
+//   "green",
+//   "blue",
+//   "purple",
+//   "indigo",
+//   "violet",
+// ];
 
-const printColor = function (box) {
-  console.log(this);
-  console.log(this.style.backgroundColor);
-};
+// const printColor = function (box) {
+//   console.log(this);
+//   console.log(this.style.backgroundColor);
+// };
 
-const changeColor = function (evt) {
-  console.log(evt);
-  const h1 = document.querySelector("h1");
-  h1.style.color = this.style.backgroundColor;
-};
+// const changeColor = function (evt) {
+//   console.log(evt);
+//   const h1 = document.querySelector("h1");
+//   h1.style.color = this.style.backgroundColor;
+// };
 
-const container = document.querySelector("#boxes");
+// const container = document.querySelector("#boxes");
 
-for (let color of colors) {
-  const box = document.createElement("div");
-  box.style.backgroundColor = color;
-  box.classList.add("box");
-  container.appendChild(box);
+// for (let color of colors) {
+//   const box = document.createElement("div");
+//   box.style.backgroundColor = color;
+//   box.classList.add("box");
+//   container.appendChild(box);
 
-  // box.addEventListener("click", function () {
-  //   printColor(box);
-  // });
+//   // box.addEventListener("click", function () {
+//   //   printColor(box);
+//   // });
 
-  // box.addEventListener("click", printColor);
+//   // box.addEventListener("click", printColor);
 
-  box.addEventListener("click", changeColor);
-}
+//   box.addEventListener("click", changeColor);
+// }
 
-document.body.addEventListener("keypress", function (evt) {
-  console.log(evt);
+// document.body.addEventListener("keypress", function (evt) {
+//   console.log(evt);
+// });
+
+const input = document.querySelector("#username");
+
+input.addEventListener("keydown", function (e) {
+  console.log("KEY DOWN");
+});
+
+input.addEventListener("keyup", function (e) {
+  console.log("KEY UP");
+});
+
+input.addEventListener("keypress", function (e) {
+  console.log("KEY PRESS");
+});
+
+const addItemInput = document.querySelector("#addItem");
+const itemsUL = document.querySelector("#items");
+
+addItemInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    if (!this.value) return;
+    // console.log(this.value);
+
+    const newItemText = this.value;
+    const newItem = document.createElement("li");
+    newItem.innerText = newItemText;
+    itemsUL.appendChild(newItem);
+    this.value = "";
+  }
 });
