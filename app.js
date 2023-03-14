0691e882198bae74fe82cc1282047aa16c57fbd6
SanazBHMN <sanazbahmani35@gmail.com>
2023-03-14 15:44:53 +0330

    Form events and preventDefault

--- app.js
+++ app.js
@@ -88,52 +88,65 @@
 // });
 
 // Coin Game
-function isTouching(a, b) {
-  const aRect = a.getBoundingClientRect();
-  const bRect = b.getBoundingClientRect();
+// function isTouching(a, b) {
+//   const aRect = a.getBoundingClientRect();
+//   const bRect = b.getBoundingClientRect();
 
-  return !(
-    aRect.top + aRect.height < bRect.top ||
-    aRect.top > bRect.top + bRect.height ||
-    aRect.left + aRect.width < bRect.left ||
-    aRect.left > bRect.left + bRect.width
-  );
-}
+//   return !(
+//     aRect.top + aRect.height < bRect.top ||
+//     aRect.top > bRect.top + bRect.height ||
+//     aRect.left + aRect.width < bRect.left ||
+//     aRect.left > bRect.left + bRect.width
+//   );
+// }
 
-const avatar = document.querySelector("#player");
-const coin = document.querySelector("#coin");
+// const avatar = document.querySelector("#player");
+// const coin = document.querySelector("#coin");
 
-window.addEventListener("keyup", function (e) {
-  // console.log(e.key);
-  if (e.key === "ArrowDown" || e.key === "Down") {
-    const currentTop = extractPosition(avatar.style.top);
-    avatar.style.top = `${currentTop + 50}px`;
-  } else if (e.key === "ArrowUp" || e.key === "Up") {
-    const currentTop = extractPosition(avatar.style.top);
-    avatar.style.top = `${currentTop - 50}px`;
-  } else if (e.key === "ArrowRight" || e.key === "Right") {
-    const currentLeft = extractPosition(avatar.style.left);
-    avatar.style.left = `${currentLeft + 50}px`;
-    avatar.style.transform = "scale(1, 1)";
-  } else if (e.key === "ArrowLeft" || e.key === "Left") {
-    const currentLeft = extractPosition(avatar.style.left);
-    avatar.style.left = `${currentLeft - 50}px`;
-    avatar.style.transform = "scale(-1, 1)";
-  }
-  if (isTouching(avatar, coin)) moveCoin();
+// window.addEventListener("keyup", function (e) {
+//   // console.log(e.key);
+//   if (e.key === "ArrowDown" || e.key === "Down") {
+//     const currentTop = extractPosition(avatar.style.top);
+//     avatar.style.top = `${currentTop + 50}px`;
+//   } else if (e.key === "ArrowUp" || e.key === "Up") {
+//     const currentTop = extractPosition(avatar.style.top);
+//     avatar.style.top = `${currentTop - 50}px`;
+//   } else if (e.key === "ArrowRight" || e.key === "Right") {
+//     const currentLeft = extractPosition(avatar.style.left);
+//     avatar.style.left = `${currentLeft + 50}px`;
+//     avatar.style.transform = "scale(1, 1)";
+//   } else if (e.key === "ArrowLeft" || e.key === "Left") {
+//     const currentLeft = extractPosition(avatar.style.left);
+//     avatar.style.left = `${currentLeft - 50}px`;
+//     avatar.style.transform = "scale(-1, 1)";
+//   }
+//   if (isTouching(avatar, coin)) moveCoin();
+// });
+
+// const extractPosition = (pos) => {
+//   if (!pos) return 100;
+//   return parseInt(pos.slice(0, -2));
+// };
+
+// const moveCoin = () => {
+//   const x = Math.floor(Math.random() * window.innerWidth);
+//   const y = Math.floor(Math.random() * window.innerHeight);
+
+//   coin.style.top = `${y}px`;
+//   coin.style.left = `${x}px`;
+// };
+
+// moveCoin();
+
+const form = document.querySelector("#signup-form");
+const creditCardInput = document.querySelector("#cc");
+const termsCheckbox = document.querySelector("#terms");
+const veggieSelect = document.querySelector("#veggie");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("SUBMITTED THE FORM!");
+  console.log("cc", creditCardInput.value);
+  console.log("terms", termsCheckbox.checked);
+  console.log("veggies", veggieSelect.value);
 });
-
-const extractPosition = (pos) => {
-  if (!pos) return 100;
-  return parseInt(pos.slice(0, -2));
-};
-
-const moveCoin = () => {
-  const x = Math.floor(Math.random() * window.innerWidth);
-  const y = Math.floor(Math.random() * window.innerHeight);
-
-  coin.style.top = `${y}px`;
-  coin.style.left = `${x}px`;
-};
-
-moveCoin();
