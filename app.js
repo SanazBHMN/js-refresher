888f3b6b3e82d136d347e42b74ef89821578dfa9
SanazBHMN <sanazbahmani35@gmail.com>
2023-04-16 09:53:49 +0330

    Returning promises from functions

--- app.js
+++ app.js
@@ -119,17 +119,38 @@
 // );
 
 // Introducing Promises
-const willGetYouADog = new Promise((resolve, reject) => {
-  const rand = Math.random();
-  if (rand < 0.5) {
-    resolve();
-  } else {
-    reject();
-  }
-});
-willGetYouADog.then(() => {
-  console.log("YAY WE GOT A DOG");
-});
-willGetYouADog.catch(() => {
-  console.log(":( NO DOG");
-});
+// const willGetYouADog = new Promise((resolve, reject) => {
+//   const rand = Math.random();
+//   if (rand < 0.5) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
+// willGetYouADog.then(() => {
+//   console.log("YAY WE GOT A DOG");
+// });
+// willGetYouADog.catch(() => {
+//   console.log(":( NO DOG");
+// });
+
+const makeDogPromise = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const rand = Math.random();
+      if (rand < 0.5) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 5000);
+  });
+};
+
+makeDogPromise()
+  .then(() => {
+    console.log("YAY WE GOT A DOG");
+  })
+  .catch(() => {
+    console.log(":( NO DOG");
+  });
