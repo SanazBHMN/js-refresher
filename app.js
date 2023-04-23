90d04bd2dfafd3bcb8f78c4426828d54e5fb7a38
SanazBHMN <sanazbahmani35@gmail.com>
2023-04-23 11:22:47 +0330

    XMLHttpRequests: chaining requests

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const xhr = new XMLHttpRequest();
 
 xhr.addEventListener("load", () => {
-  console.log("IT WORKED!");
+  console.log("FIRST REQUEST WORKED!");
   const data = JSON.parse(xhr.responseText);
-  for (let planet of data.results) {
-    console.log(planet.name);
-  }
+  const filmURL = data.results[0].films[0];
+  const filmReq = new XMLHttpRequest();
+  filmReq.addEventListener("load", () => {
+    console.log("SECOND REQUEST WORKED!");
+    const filmData = JSON.parse(filmReq.responseText);
+    console.log(filmData);
+  });
+  filmReq.addEventListener("error", (e) => {
+    console.log("ERROR!", e);
+  });
+  filmReq.open("GET", filmURL);
+  filmReq.send();
+
+  // for (let planet of data.results) {
+  //   console.log(planet.name);
+  // }
 });
 
 xhr.addEventListener("error", () => {
