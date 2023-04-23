5822698fc0210de61ace547d136679511ab84818
SanazBHMN <sanazbahmani35@gmail.com>
2023-04-23 12:32:46 +0330

    Chaining fetch requests

--- app.js
+++ app.js
@@ -27,19 +27,43 @@
 
 // console.log("Request Sent!");
 
-fetch("https://swapi.dev/api/planetssuy21/")
+// fetch("https://swapi.dev/api/planets/")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`Status Code Error: ${response.status}`);
+//     } else {
+//       response.json().then((data) => {
+//         for (let planet of data.results) {
+//           console.log(planet);
+//         }
+//       });
+//     }
+//   })
+//   .catch((err) => {
+//     console.log("SOMETHING WENT WRONG WITH FETCH");
+//     console.log(err);
+//   });
+
+fetch("https://swapi.dev/api/planets/")
   .then((response) => {
     if (!response.ok) {
-      throw new Error(`Status Code Error: ${response.status}`);
-    } else {
-      response.json().then((data) => {
-        for (let planet of data.results) {
-          console.log(planet.name);
-        }
-      });
+      throw new Error(`Status Code: ${response.status}`);
     }
+    return response.json();
+  })
+  .then((data) => {
+    const filmURL = data.results[0].films[0];
+    return fetch(filmURL);
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Status Code: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
   })
   .catch((err) => {
-    console.log("SOMETHING WENT WRONG WITH FETCH");
     console.log(err);
   });
