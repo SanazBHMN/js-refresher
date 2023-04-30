82af7669d5f54cedb56328edbc1f73f34e510b1a
SanazBHMN <sanazbahmani35@gmail.com>
2023-04-30 14:05:06 +0330

    Parallel vs sequential requests

--- app.js
+++ app.js
@@ -42,15 +42,69 @@
 
 // getTodos();
 
-async function getTodos() {
-  try {
-    const res = await axios.get(
-      "https://jsonplaceholderasdfa.typicode.com/todos/1"
-    );
-    console.log(res.data);
-  } catch (e) {
-    console.log("IN CATCH!", e);
-  }
+// async function getTodos() {
+//   try {
+//     const res = await axios.get(
+//       "https://jsonplaceholderasdfa.typicode.com/todos/1"
+//     );
+//     console.log(res.data);
+//   } catch (e) {
+//     console.log("IN CATCH!", e);
+//   }
+// }
+
+// getTodos();
+
+// SEQUENTIAL REQUESTS
+// async function get3Pokemon() {
+//   const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
+//   const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//   const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// PARALLEL REQUESTS
+// async function get3Pokemon() {
+//   const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+//   const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//   const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//   const poke1 = await prom1;
+//   const poke2 = await prom2;
+//   const poke3 = await prom3;
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// get3Pokemon();
+
+function changeBodyColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
 }
 
-getTodos();
+// async function lightShow() {
+//   await changeBodyColor("teal", 1000);
+//   await changeBodyColor("pink", 1000);
+//   await changeBodyColor("indigo", 1000);
+//   await changeBodyColor("violet", 1000);
+// }
+
+async function lightShow() {
+  const p1 = changeBodyColor("teal", 1000);
+  const p2 = changeBodyColor("pink", 1000);
+  const p3 = changeBodyColor("indigo", 1000);
+  const p4 = changeBodyColor("violet", 1000);
+  await p1;
+  await p2;
+  await p3;
+  await p4;
+}
+
+lightShow();
