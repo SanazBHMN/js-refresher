281518393c5ffab352884d86a243ebf0bd8eb61a
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-05-20 11:18:34 +0330

    JS classes - syntactical sugar

--- app.js
+++ app.js
@@ -26,31 +26,59 @@
 
 // const firstColor = makeColor(35, 255, 150);
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
 
-  // console.log(this);
+//   // console.log(this);
+// }
+
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgba(${r}, ${g}, ${b}, ${a})`;
+// };
+
+// const color1 = new Color(40, 255, 60);
+// const color2 = new Color(0, 0, 0);
+
+// document.body.style.background = color1.rgb();
+// document.body.style.background = color1.rgba(0.3);
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
 }
 
-Color.prototype.rgb = function () {
-  const { r, g, b } = this;
-  return `rgb(${r}, ${g}, ${b})`;
-};
+const red = new Color(255, 67, 89, "tomato");
+const white = new Color(255, 255, 255);
 
-Color.prototype.hex = function () {
-  const { r, g, b } = this;
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
-
-Color.prototype.rgba = function (a = 1.0) {
-  const { r, g, b } = this;
-  return `rgba(${r}, ${g}, ${b}, ${a})`;
-};
-
-const color1 = new Color(40, 255, 60);
-const color2 = new Color(0, 0, 0);
-
-document.body.style.background = color1.rgb();
-document.body.style.background = color1.rgba(0.3);
+document.body.style.backgroundColor = red.rgba(0.4);
